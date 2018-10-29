7e61b2c1ac82ecc3cc665f7a30de208a5de035eb
fishyFrogFace <camillmg@stud.ntnu.no>
2018-10-29 13:40:12 +0100

    Add word length count

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 module Lib
-    ( someFunc
+    ( byLength
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+byLength :: [[a]] -> [Int]
+byLength []  = []
+byLength lst = let genres = 0:genres in bl lst genres
+
+bl :: [[a]] -> [Int] -> [Int]
+bl [] res     = res
+bl (x:xs) res = bl xs $ update 0 (length x) res
+
+update :: Int -> Int -> [Int] -> [Int]
+update inc n (x:xs)
+  | inc == n  = x+1:xs
+  | otherwise = x:update (inc+1) n xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+import Test.QuickCheck
+import Test.Hspec 
+import Lib
+
+genPos :: Gen Int
+genPos = choose (0, 1000)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+        describe "byLength" $ do
+          it "counts length of successively longer lists" $ do
+            property $ forAll genPos $ \n -> (take n $ byLength $ take n $ iterate (0:) []) == replicate n 1
+          it "returns empty list for empty input" $ do
+            byLength [] `shouldBe` []
+          it "counts occurrences" $ do
+            property $ forAll genPos $
+              \n -> (take (n+3) $ byLength [[1..n], [], [1..n], [], []]) == 3:replicate (n-1) 0 ++ [2,0,0]
