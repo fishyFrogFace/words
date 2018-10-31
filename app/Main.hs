a6b6209e042e40f81c9c9b2f1093dbb9e2c3cbbb
fishyFrogFace <camillmg@stud.ntnu.no>
2018-10-31 05:19:58 +0100

    Help message, word count, index count, display words of given length

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,45 @@
 module Main where
 
-import System.Environment
+import System.Environment (getArgs)
+import Data.Char (isDigit)
 import Lib
 
-count :: String -> IO ()
-count x = do
+help :: String
+help = "\nhelp         - display this list of commands" ++
+       "\ndistribution - display word length distribution" ++
+       "\ncountAll     - count the words in the file" ++
+       "\ncountIndex n - count words of length n" ++
+       "\nshowIndex n  - display all words of length n"
+
+areDigits :: String -> Bool
+areDigits []     = True
+areDigits (x:xs)
+  | isDigit x = areDigits xs
+  | otherwise = False
+
+process :: String -> IO ()
+process x = do
   file <- readFile x
-  print . take 50 $ byLength $ lines file
+  let allWords = lines file
+  let sorted = take 50 $ byLength $ allWords
+  putStrLn "Type 'help' to see a list of commands\n"
+  action <- getLine
+  case words action of
+    ["help"]          -> putStrLn help
+    ["distribution"]  -> print $ map length sorted
+    ["countAll"]      -> print $ length allWords
+    ["countIndex", n] -> if areDigits n then
+                           print $ length $ sorted !! (read n :: Int)
+                         else
+                           print "Not a valid index\n"
+    ["showIndex", n]  -> if areDigits n then
+                           print $ sorted !! (read n :: Int)
+                         else
+                           print "Not a valid index"
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [x] -> count x
+    [x] -> process x
     _   -> putStrLn "You need to provide a filename"
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,15 +2,16 @@ module Lib
     ( byLength
     ) where
 
-byLength :: [[a]] -> [Int]
+byLength :: [[a]] -> [[[a]]]
 byLength []  = []
-byLength lst = let genres = 0:genres in bl lst genres
+byLength lst = let genres = []:genres in bl lst genres
 
-bl :: [[a]] -> [Int] -> [Int]
+bl :: [[a]] -> [[[a]]] -> [[[a]]]
 bl [] res     = res
-bl (x:xs) res = bl xs $ update 0 (length x) res
+bl (x:xs) res = bl xs $ update 0 x res
 
-update :: Int -> Int -> [Int] -> [Int]
+update :: Int -> [a] -> [[[a]]] -> [[[a]]]
 update inc n (x:xs)
-  | inc == n  = x+1:xs
-  | otherwise = x:update (inc+1) n xs
+  | inc == (length n)  = (n:x):xs
+  | otherwise        = x:update (inc+1) n xs
+
